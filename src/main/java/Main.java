8b219f6152139a6aa6d8f94d9a0b40c6f0e4f819
kairat-kempirbaev <7647704+kairat-kempirbaev@users.noreply.github.com>
2022-12-05 12:41:47 -0800

    Connect Javalin to local/aws PostgreSQL database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,44 @@
 import io.javalin.Javalin;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.sql.*;
 public class Main {
-    public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.get("/login", ctx -> ctx.result("Trying to log in"));
-        app.get("/user/*", ctx -> {
-            ctx.result("You are here because " + ctx.path().split("/")[2] + " matches " );
-        });
-        app.get("/admin/approve", ctx -> ctx.result("Trying to approve reimbursement"));
-        app.get("/admin/view", ctx -> ctx.result("Get all reimbursements"));
+    public static void main(String[] args)  {
+        String propertiesPath = "src/main/resources/app.properties";
+        Properties appProps = new Properties();
+        FileInputStream propInput;
+        Connection c = null;
+        try{
+            propInput = new FileInputStream(propertiesPath);
+            appProps.load(propInput);
+            String db_src_url = appProps.getProperty("URL");
+            String db_name = appProps.getProperty("DB");
+            String db_user = appProps.getProperty("USER");
+            String db_pass = appProps.getProperty("PASSWORD");
+            Class.forName("org.postgresql.Driver");
+            String URL = db_src_url + '/' + db_name;
+            Connection conn = DriverManager.getConnection(URL, db_user, db_pass);
+            Javalin app = Javalin.create().start(7000);
+            app.get("/", ctx -> ctx.result("Hello World"));
+            app.get("/login", ctx -> ctx.result("Trying to log in"));
+            app.get("/user/:name", ctx -> {
+                ctx.result(ctx.pathParam("name") );
+            });
+            app.get("/admin/approve", ctx -> ctx.result("Trying to approve reimbursement"));
+            app.get("/admin/view", ctx -> ctx.result("Get all reimbursements"));
+        }catch (SQLException e){
+            System.out.println("No way to recover, Database error");
+            throw new RuntimeException();
+        } catch (IOException e) {
+            System.out.println("Can't read properties");
+            throw new RuntimeException();
+        } catch (ClassNotFoundException e) {
+            System.out.println("No psql driver");
+            throw new RuntimeException();
+        }
     }
 }
 
